1c93d52ea5b93342b1bbeb21f87ca1ae3d3a8f7c
Boontharik Keawbenjanukun <TheBaka_0749@hotmail.com>
2021-11-22 21:59:44 +0700

    พิซซ่าและวัตถุดิบ เสร็จ

--- src/routes/ingredients.ts
+++ src/routes/ingredients.ts
@@ -1,26 +1,138 @@
 import { Application, Request, Response } from "express";
 import Ingredient from "../models/Ingredient";
+import { body, validationResult, param, CustomValidator } from 'express-validator';
+import { Op } from "sequelize";
+
+const isValidIngredientFromId: CustomValidator = async id => {
+    const ingredient = await Ingredient.findOne({
+        where: { id: id }
+    });
+
+    if (!ingredient) {
+        throw new Error(`Ingredient id ${id} does not exists.`);
+    }
+};
+
+const isIngredientDuplicateName: CustomValidator = async name => {
+    const ingredient = await Ingredient.findOne({
+        where: { name: name }
+    });
+
+    if (ingredient) {
+        throw new Error(`You already have a ingredient with ${name} name.`);
+    }
+}
+
+const isIngredientDuplicateNameIgnoreId: CustomValidator = async (name, obj) => {
+    const id = obj.req.params?.id;
+
+    const ingredient = await Ingredient.findOne({
+        where: {
+            [Op.and]: [{
+                name: name,
+            }, {
+                id: {
+                    [Op.ne]: id,
+                }
+            }],
+        },
+    });
+
+    if (ingredient) {
+        throw new Error(`You already have a ingredient with ${name} name.`);
+    }
+}
 
 export default function (app: Application) {
-    // Routes
-    // - GET /ingredients
-    // - GET /ingredients/:id
-    // - POST /ingredients
-    // - PUT /ingredients/:id
-    // - DELETE /ingredients/:id
 
     app.get('/ingredients', async (req: Request, res: Response) => {
         const ingredients = await Ingredient.findAll();
         res.send(ingredients);
-        // res.send([]);
     });
 
-    // Other routes
+    app.get(
+        '/ingredients/:id',
+        param('id').custom(isValidIngredientFromId),
 
-    // const ingredient = new Ingredient({
-    //     name: 'Garlic!',
-    //     price: 5,
-    // });
-    // ingredient.save();
+        async (req: Request, res: Response) => {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
+
+            const { id } = req.params;
+
+            const ingredient = await Ingredient.findOne({
+                where: { id: id }
+            });
+
+            res.send(ingredient);
+        }
+    );
+
+    app.post(
+        '/ingredients',
+        body('name').not().isEmpty().trim().escape().custom(isIngredientDuplicateName),
+
+        async (req: Request, res: Response) => {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
+
+            const { name, price } = req.body;
+
+            const ingredient = await Ingredient.create({
+                name,
+                price
+            });
+
+            res.send(ingredient);
+        }
+    );
+
+    app.put(
+        "/ingredients/:id",
+        param('id').custom(isValidIngredientFromId),
+        body('name').not().isEmpty().trim().escape().custom(isIngredientDuplicateNameIgnoreId),
+
+        async (req: Request, res: Response) => {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
+
+            const { name, price } = req.body;
+            const { id } = req.params;
+
+            const ingredient = await Ingredient.update({
+                name: name,
+                price: price
+            }, {
+                where: { id: id }
+            });
+
+            res.send(ingredient);
+        }
+    );
+
+    app.delete(
+        '/ingredients/:id',
+        param('id').custom(isValidIngredientFromId),
+        async (req: Request, res: Response) => {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
+
+            const { id } = req.params;
+
+            await Ingredient.destroy({
+                where: { id: id }
+            });
+
+            res.send([]);
+        }
+    );
 
 }
--- src/routes/pizzas.ts
+++ src/routes/pizzas.ts
@@ -1,75 +1,185 @@
 import { Application, Request, Response } from "express";
 import Pizza from './../models/Pizza';
+import { body, validationResult, param, CustomValidator } from 'express-validator';
+import { Op } from "sequelize";
+
+const isValidPizzaFromId: CustomValidator = async id => {
+    const pizza = await Pizza.findOne({
+        where: { id: id }
+    });
+
+    if (!pizza) {
+        throw new Error(`Pizza id ${id} does not exists.`);
+    }
+};
+
+// isPizzaDuplicateName
+const isPizzaDuplicateName: CustomValidator = async name => {
+    const pizza = await Pizza.findOne({
+        where: { name: name }
+    });
+    if (pizza) {
+        throw new Error(`You already have a pizza with ${name} name.`);
+    }
+}
+
+const isPizzaDuplicateNameIgnoreId: CustomValidator = async (name, id) => {
+    const pizza = await Pizza.findOne({
+        where: { name: name }
+    });
+
+    if (pizza) {
+        throw new Error(`You already have a pizza with ${name} name.`);
+    }
+}
 
 export default function (app: Application) {
-    // Routes
-    // - GET /pizzas
-    // - GET /pizzas/:id
-    // - POST /pizzas
-    // - PUT /pizzas/:id
-    // - DELETE /pizzas/:id
-
-    // https://www.npmjs.com/package/sequelize-typescript
-
     app.get('/pizzas', async (req: Request, res: Response) => {
         const pizzas = await Pizza.findAll();
         res.send(pizzas);
     });
 
-    app.post("/pizzas", async (req: Request, res: Response) => {
-        // อ่านค่าจาก request
-        const { name } = req.body;
+    app.get(
+        '/pizzas/:id',
 
-        // สร้าง
-        const pizza = await Pizza.create({
-            name: name
-        });
+        // https://express-validator.github.io/docs/
+        // Validation Logic
+        param('id').custom(isValidPizzaFromId),
 
-        // ส่ง response กลับ
-        res.send(pizza);
-    });
+        async (req: Request, res: Response) => {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
+
+            // อ่านค่าจาก request
+            const { id } = req.params;
+
+            // สร้าง
+            const pizza = await Pizza.findOne({
+                where: { id: id }
+            });
+
+            // ส่ง response กลับ
+            res.send(pizza);
+        }
+    );
+
+    app.post(
+        '/pizzas',
+        // "Hawaii"
+        // ["          665554412        "]
+        // \\ \ %2F " "
+        // Sanitize data
+        body('name').not().isEmpty().trim().escape().custom(isPizzaDuplicateName),
+
+        // Homework
+        // 1. เพิ่มการเช็คว่าชื่อห้ามซ้ำ (ใช้ custom validator function)
+
+        async (req: Request, res: Response) => {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
+
+            // อ่านค่าจาก request
+            const { name } = req.body;
+
+            // สร้าง
+            const pizza = await Pizza.create({
+                name: name
+            });
+
+            // ส่ง response กลับ
+            res.send(pizza);
+        }
+    );
 
     // request url (params)
     // request body
-    app.put("/pizzas/:id", async (req: Request, res: Response) => {
-        // อ่านค่าจาก request
-        const { name } = req.body;
-        const { id } = req.params;
+    app.put(
+        "/pizzas/:id",
+        param('id').custom(isValidPizzaFromId), // ID มีอยู่จริง
+        body('name').not().isEmpty().trim().escape().custom(async (name, obj) => {
+            const id = obj.req.params?.id;
 
-        // สร้าง
-        const pizza = await Pizza.update({
-            name: name,
-        }, {
-            where: { id: id }
-        });
+            // Find the one that has name = name, but not the one that has id = id
 
-        // ส่ง response กลับ
-        res.send(pizza);
-    });
+            // https://sequelize.org/master/manual/model-querying-basics.html
+            // Operators
+            // SELECT * FROM pizzas WHERE id = 5
+            // SELECT * FROM pizzas WHERE name = 'Q'
+            // SELECT * FROM pizzas WHERE name = 'Q' AND id != 9
+            // const pizza = await Pizza.findOne({
+            //     where: { name: name },
+            // });
 
-    app.delete("/pizzas/:id", async (req: Request, res: Response) => {
-        // อ่านค่าจาก request
-        const { id } = req.params;
+            const pizza = await Pizza.findOne({
+                where: {
+                    [Op.and]: [{
+                        name: name,
+                    }, {
+                        id: {
+                            [Op.ne]: id,
+                        }
+                    }],
+                },
+            });
 
-        // สร้าง
-        await Pizza.destroy({
-            where: { id: id }
-        });
+            if (pizza) {
+                throw new Error(`You already have a pizza with ${name} name.`);
+            }
+        }),
+        // Homework 2
+        // เพิ่ม duplicate name check เช่นกัน แต่ให้มัน ignore id ของตัวเอง
 
-        // ส่ง response กลับ
-        res.send([]);
-    });
+        // id = 2
+        // name = Hawaii
 
-    app.get("/pizzas/:id", async (req: Request, res: Response) => {
-        // อ่านค่าจาก request
-        const { id } = req.params;
+        // 
 
-        // สร้าง
-        const pizza = await Pizza.findOne({
-            where: { id: id }
-        });
+        // Homework 3 คือทำทุกอย่างตามไฟล์นี้ แต่เป็นสำหรับ ingredient
+        async (req: Request, res: Response) => {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
 
-        // ส่ง response กลับ
-        res.send(pizza);
-    });
+            // อ่านค่าจาก request
+            const { name } = req.body;
+            const { id } = req.params;
+
+            // สร้าง
+            const pizza = await Pizza.update({
+                name: name,
+            }, {
+                where: { id: id }
+            });
+
+            // ส่ง response กลับ
+            res.send(pizza);
+        }
+    );
+
+    app.delete(
+        '/pizzas/:id',
+        param('id').custom(isValidPizzaFromId),
+        async (req: Request, res: Response) => {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
+
+            // อ่านค่าจาก request
+            const { id } = req.params;
+
+            // สร้าง
+            await Pizza.destroy({
+                where: { id: id }
+            });
+
+            // ส่ง response กลับ
+            res.send([]);
+        }
+    );
 }
